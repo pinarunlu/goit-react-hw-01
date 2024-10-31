38974471de3125ce9ba211c7d403373c99de3e1d
pınar <pinar.dogans96@gmail.com>
2024-10-31 18:11:49 +0300

    hw react 1 oluşturuldu

---
+++ first react project/src/App.jsx
@@ -0,0 +1,20 @@
+
+import React from "react";
+import Profile from "./components/Profile";
+import userData from "./userData.json";
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+};
+
+export default App;
