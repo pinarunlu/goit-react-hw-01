9fa00c48d783e5f1ede09032f453ea532ab33ac0
pınar <pinar.dogans96@gmail.com>
2024-11-01 17:09:55 +0300

    react HW1 first task is done

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -6,26 +6,30 @@ const Profile = ({ name, tag, location, image, stats }) => {
   return (
     <div className={styles.profile}>
       <div className={styles.description}>
-        <img src={image} alt="User avatar" className={styles.avatar} />
+        <img className={styles.avatar} src={image} alt="User avatar"  />
         <p className={styles.name}>{name}</p>
         <p className={styles.tag}>@{tag}</p>
         <p className={styles.location}>{location}</p>
-      </div>
-      <ul className={styles.stats}>
-        <li>
+          </div>
+      <ul className={styles.box}>
+        
+        <li className={styles.item}>
           <span className={styles.label}>Followers</span>
           <span className={styles.quantity}>{stats.followers}</span>
         </li>
-        <li>
+        <li className={styles.item}>
           <span className={styles.label}>Views</span>
           <span className={styles.quantity}>{stats.views}</span>
         </li>
-        <li>
+        <li className={styles.item}>
           <span className={styles.label}>Likes</span>
           <span className={styles.quantity}>{stats.likes}</span>
-        </li>
+            </li>
+         
       </ul>
-    </div>
+      </div>
+      
+  
   );
 };
 
