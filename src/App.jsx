fc4e64e2d62d5ab51b2087326c4dd0affa8d3de9
pınar <pinar.dogans96@gmail.com>
2024-11-01 21:31:52 +0300

    react HW second task is done

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,27 @@
 import React from "react";
 import Profile from "./components/Profile";
 import userData from "./userData.json";
+import FriendList from './components/FriendList';
+import friends from './friends.json';
 
 const App = () => {
   return (
     <>
+      {/* Profile Section */}
+      <div className="profile">
       <Profile
         name={userData.username}
         tag={userData.tag}
         location={userData.location}
         image={userData.avatar}
         stats={userData.stats}
-      />
+        />
+      </div>
+      {/* FriendsList Section */}
+      <div className="friends">
+         <FriendList friends={friends} />
+        
+      </div>
     </>
   );
 };
---
+++ src/components/FriendList.jsx
@@ -0,0 +1,21 @@
+// src/components/FriendList/FriendList.jsx
+import FriendListItem from './FriendListItem';
+import styles from './FriendList.module.css';
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={styles.friendList}>
+      {friends.map(friend => (
+        <li key={friend.id}>
+          <FriendListItem
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default FriendList;
